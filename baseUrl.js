ef879d05262c81ddde91d7806778d99444fa4fdd
SyedHammad2000 <116845584+SyedHammad2000@users.noreply.github.com>
2024-02-02 19:42:38 +0500

    Update baseUrl.js

--- baseUrl.js
+++ baseUrl.js
@@ -1,4 +1,4 @@
 const baseUrl =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? "https://24-hourshop.vercel.app" : "http://localhost:3000";
 
 export default baseUrl;
