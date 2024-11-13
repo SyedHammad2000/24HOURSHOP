14154fc450e80dade617c09e6ae13d074349a040
SyedHammad2000 <Hammadimran736@gmail.com>
2024-11-13 23:08:07 +0500

    Update Layout.jsx

--- component/Layout.jsx
+++ component/Layout.jsx
@@ -5,14 +5,14 @@ import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Navbar />
 
       <Toaster position="top-center" />
       {children}
 
       <Footer />
-    </>
+    </div>
   );
 };
 
