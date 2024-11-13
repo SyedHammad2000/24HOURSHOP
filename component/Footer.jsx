39817f6c9c67b571ea9462045752478ebe39de72
SyedHammad2000 <Hammadimran736@gmail.com>
2024-11-13 23:08:59 +0500

    Update Footer.jsx

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-center" style={{background:"black",color:"azure",fontSize:"1.4rem"}}>
-      <h1 >Copyright © 2024. All rights reserved.</h1>
+    <div
+      className="text-center"
+      style={{
+        background: "black",
+        color: "azure",
+        fontSize: "1.4rem",
+        height: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Copyright © 2024. All rights reserved.</h1>
     </div>
   );
 };
